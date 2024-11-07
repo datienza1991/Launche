4f7ec0ac642dab203cb0e36dc2aa4c51c2f48e7f
Darren Atienza <datienza@doehle-mnl.com>
2024-11-07 18:06:38 +0800

    #65 Move other functionality to Presenter

--- UI/MainWindowPresenter.cs
+++ UI/MainWindowPresenter.cs
@@ -1,4 +1,5 @@
 ﻿using ApplicationCore.Features.DevApps;
+using ApplicationCore.Features.Projects;
 using Infrastructure.ViewModels;
 using Microsoft.Win32;
 using System.Windows;
@@ -9,9 +10,15 @@ namespace UI.MainWindowx.Presenter
     {
         event EventHandler OpenDevApp;
         event EventHandler FetchDevAppsEvent;
+        event EventHandler DeleteDevAppEvent;
+        event EventHandler NewProjectEvent;
+        event EventHandler OpenProjectDialog;
+        event EventHandler DeleteProjectEvent;
         string DevAppFilePath { get; set; }
         IAddDevAppService? AddDevAppService { get; }
         IGetAllDevAppService? GetAllDevAppService { get; }
+        IDeleteDevAppService? DeleteDevAppService { get; }
+        IDeleteProjectService? DeleteProjectService { get; }
         MainWindowViewModel MainWindowViewModel { get; set; }
     }
 
@@ -24,6 +31,10 @@ namespace UI.MainWindowx.Presenter
 
             presenter.OpenDevApp += Presenter_OpenDevApp;
             presenter.FetchDevAppsEvent += Presenter_FetchDevAppsEvent;
+            presenter.DeleteDevAppEvent += Presenter_DeleteDevAppEvent;
+            presenter.NewProjectEvent += Presenter_NewProjectEvent;
+            presenter.OpenProjectDialog += Presenter_OpenProjectDialog;
+            presenter.DeleteProjectEvent += Presenter_DeleteProjectEvent;
             this.presenter = presenter;
             if (this.presenter.MainWindowViewModel is null)
             {
@@ -32,6 +43,55 @@ namespace UI.MainWindowx.Presenter
             else
             {
                 MessageBox.Show("Instance of MainWindowViewModel was created earlier!");
+            }
+        }
+
+        private async void Presenter_DeleteProjectEvent(object? sender, EventArgs e)
+        {
+            if (this.presenter.MainWindowViewModel!.SelectedProjectPath!.Id == 0) return;
+
+            var result = await this.presenter.DeleteProjectService!.Delete(this.presenter.MainWindowViewModel!.SelectedProjectPath!.Id);
+
+            if (result)
+            {
+                //await Search();
+                this.presenter.MainWindowViewModel!.SelectedProjectPath = new();
+                this.presenter.MainWindowViewModel!.SelectedIdePath = new();
+            }
+        }
+
+        private void Presenter_OpenProjectDialog(object? sender, EventArgs e)
+        {
+            var openFolderDialog = new OpenFolderDialog();
+            var result = openFolderDialog.ShowDialog() ?? false;
+
+            if (result)
+            {
+                string filePath = openFolderDialog.FolderName;
+                string name = openFolderDialog.SafeFolderName;
+                var project = this.presenter.MainWindowViewModel!.SelectedProjectPath!.Copy();
+                project.Name = name;
+                project.Path = filePath;
+                this.presenter.MainWindowViewModel!.SelectedProjectPath = project;
+            }
+        }
+
+        private void Presenter_NewProjectEvent(object? sender, EventArgs e)
+        {
+            this.presenter.MainWindowViewModel!.SelectedProjectPath = new();
+            this.presenter.MainWindowViewModel!.SelectedIdePath = new();
+        }
+
+        private async void Presenter_DeleteDevAppEvent(object? sender, EventArgs e)
+        {
+            if (this.presenter.MainWindowViewModel!.SelectedIdePath!.Id == 0) return;
+
+            var result = await this.presenter.DeleteDevAppService!.Delete(new() { Id = this.presenter.MainWindowViewModel!.SelectedIdePath!.Id });
+
+            if (result)
+            {
+                var getAllDevAppVm = await presenter.GetAllDevAppService!.Handle();
+                presenter.MainWindowViewModel!.IdePathsModels = [.. getAllDevAppVm.DevApps];
             }
         }
 
